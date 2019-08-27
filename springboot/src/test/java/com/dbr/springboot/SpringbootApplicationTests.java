2a66ea7b133d2ae4f80fca4d95203a2afd002c78
doubleReal <2675258581@qq.com>
2019-08-27 21:03:33 +0800

    测试

--- springboot/src/test/java/com/dbr/springboot/SpringbootApplicationTests.java
+++ springboot/src/test/java/com/dbr/springboot/SpringbootApplicationTests.java
@@ -1,5 +1,6 @@
 package com.dbr.springboot;
 
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,7 @@ public class SpringbootApplicationTests {
 
     @Test
     public void contextLoads() {
+        int a=4;
     }
 
 }
