901dadea46f5036c40d4af9c0549b6b95de3dd29
doubleReal <2675258581@qq.com>
2019-08-29 11:14:10 +0800

    更新

---
+++ springboot/src/main/java/com/dbr/springboot/controller/testController.java
@@ -0,0 +1,23 @@
+package com.dbr.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * springboot
+ * 2019/8/29 9:28
+ * dd
+ *
+ * @author zhaozheng
+ * @since
+ **/
+@Controller
+@RequestMapping("test")
+public class testController {
+    @RequestMapping("hello")
+    @ResponseBody
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
